b627f616c1e0678c21c2cfa43520de567eec9400
carlos-souza-02 <carlos.alberto.g.s@icloud.com>
2020-10-01 17:16:34 -0300

    Configuração para deploy

---
+++ build/precache-manifest.25e771cb30658fcb3984f6a0eda538e7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d73cfb2a4076fa52cb259ef6eab90cb8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d7abb5a68903e9b5be87",
+    "url": "/static/css/main.fec700b6.chunk.css"
+  },
+  {
+    "revision": "718c877f3dadb50b8c87",
+    "url": "/static/js/2.f03157c6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.f03157c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7abb5a68903e9b5be87",
+    "url": "/static/js/main.c111617e.chunk.js"
+  },
+  {
+    "revision": "50b2e1c7e0960e178e3d",
+    "url": "/static/js/runtime-main.4fb74bea.js"
+  },
+  {
+    "revision": "924a74c94e00b9c764f8e97ceef8af61",
+    "url": "/static/media/image-default.924a74c9.jpeg"
+  },
+  {
+    "revision": "f0cdea4e620c6cc66a265ee112c8599a",
+    "url": "/static/media/moon.f0cdea4e.png"
+  },
+  {
+    "revision": "f4f573c8b4a9697adb65be1dab8a7485",
+    "url": "/static/media/sun.f4f573c8.png"
+  }
+]);
