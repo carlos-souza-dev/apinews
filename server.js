88bc88a39f9323309ca998bbbc1ee33a4dfbaa89
carlos-souza-02 <carlos.alberto.g.s@icloud.com>
2020-10-03 00:09:11 -0300

    Configurando API no arq. serve.js

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const fetch = require('node-fetch');
+const cors = require('cors');
 const favicon = require('express-favicon');
 const path = require('path');
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 5000;
 const app = express();
+
+app.use(cors());
 app.use(favicon(__dirname + '/build/favicon.ico'));
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
@@ -10,6 +14,17 @@ app.use(express.static(path.join(__dirname, 'build')));
 app.get('/ping', function (req, res) {
  return res.send('pong');
 });
+
+app.get('/api', function (req, res) {
+
+    const response = fetch(`https://newsapi.org/v2/top-headlines?country=us&apiKey=ca1ce57fdd0f40a8ba1a88403a72a809`);
+    response.then((res)=>{
+      return res.json();
+    }).then((json)=>{ 
+      res.send(json)
+    })
+});
+
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
